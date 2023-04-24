4b88855e0c52393ed373ede8badd3fb7d40257dd
VINAYCHARY07 <123128034+VINAYCHARY07@users.noreply.github.com>
2023-04-24 17:23:36 -0500

    added all files

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+function(username, password, done) {
+Account.findOne({ username: username }, function (err, user) {
+if (err) { return done(err); }
+if (!user) {
+return done(null, false, { message: 'Incorrect username.' });
+}
+if (!user.validPassword(password)) {
+return done(null, false, { message: 'Incorrect password.' });
+}
+return done(null, user);
+});
+}))
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 require('dotenv').config();
 const connectionString =
 process.env.MONGO_CON
@@ -45,6 +71,14 @@ app.use('/doctor', doctorRouter);
 app.use('/board', boardRouter);
 app.use('/Selector', SelectorRouter);
 app.use('/resource', resourceRouter);
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
